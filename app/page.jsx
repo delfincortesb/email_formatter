971f3d30247c42cb6674d9bcc452d92b3748d463
Delfin <66261899+delfincortesb@users.noreply.github.com>
2023-03-10 14:51:33 -0400

    Added Favicon Fixed issue with mobile viewport Added prettier Small CSS Changes

--- app/page.jsx
+++ app/page.jsx
@@ -47,17 +47,17 @@ export default function Home() {
   }
 
   return (
-    <div className="max-w-xl mx-auto flex flex-col gap-8 w-full mt-8">
-      <h1 className="text-white font-black text-5xl text-center">
+    <div className="mx-auto mt-8 flex max-w-xs flex-col gap-8 text-center sm:max-w-lg lg:max-w-xl">
+      <h1 className="text-center text-5xl font-black text-white">
         Email List Formatter
       </h1>
-      <section className="flex flex-col w-full gap-6 ">
+      <section className="flex flex-col gap-6 ">
         <form
           onSubmit={() => formatData(data, event)}
-          className={`flex flex-col justify-around gap-1.5 w-full`}
+          className={`flex w-full flex-col justify-around gap-1.5`}
         >
           <textarea
-            className={`min-h-[200px] rounded cursor-auto outline w-full outline-[#50585F] outline-1 bg-[#0D1117] text-[#C9D1D8] p-1 text-sm focus:outline-blue-500`}
+            className={`block min-h-[200px] cursor-auto resize-y rounded bg-[#0D1117] p-1 text-sm text-[#C9D1D8] outline outline-1 outline-[#50585F] focus:outline-blue-500`}
             autoFocus
             placeholder="Paste your email list here!"
             onChange={(e) => setData(e.target.value)}
@@ -65,14 +65,14 @@ export default function Home() {
           ></textarea>
           <button
             type="submit"
-            className="bg-green-700 text-white text-sm py-2 rounded-md hover:bg-green-600 transition-colors"
+            className="rounded-md bg-green-700 py-2 text-sm text-white transition-colors hover:bg-green-600"
           >
             Submit
           </button>
         </form>
-        <div className={`flex flex-col justify-around gap-1.5 w-full`}>
+        <div className={`flex w-full flex-col justify-around gap-1.5`}>
           <textarea
-            className="w-full bg-gray-800 rounded min-h-[200px] text-[#C9D1D8] p-1 text-sm"
+            className="block min-h-[200px] resize-y rounded bg-gray-800 p-1 text-sm text-[#C9D1D8]"
             placeholder="You'll see your results here!"
             disabled
             value={results.join("\n")}
@@ -80,7 +80,7 @@ export default function Home() {
           <button
             type="button"
             onClick={copyToClipboard}
-            className="bg-gray-700 text-white text-sm py-2 rounded-md hover:outline hover:outline-blue-500 hover:outline-2 "
+            className="rounded-md bg-gray-700 py-2 text-sm text-white hover:outline hover:outline-2 hover:outline-blue-500 "
           >
             Copy
           </button>

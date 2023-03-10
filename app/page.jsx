aa88d0dd00cbee0ea9c23da96e09e7176c3bf67f
Delfin <66261899+delfincortesb@users.noreply.github.com>
2023-03-09 23:46:50 -0400

    CSS Changes

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 export default function Home() {
   const [data, setData] = useState();
   const [results, setResults] = useState([]);
+
   function formatData(data, event) {
     event.preventDefault();
 
@@ -40,53 +41,51 @@ export default function Home() {
     }
     setResults(combinedEmails);
   }
+
   function copyToClipboard() {
     return navigator.clipboard.writeText(results);
   }
+
   return (
-    <>
-      <main className="max-w-xl mx-auto flex flex-col gap-8 w-full mt-8">
-        <h1 className="text-white font-black text-5xl text-center">
-          Email List Formatter
-        </h1>
-        <section className="flex flex-col gap-6 ">
-          <form
-            onSubmit={() => formatData(data, event)}
-            className={`flex flex-col justify-around gap-1.5 w-full mx-auto`}
+    <div className="max-w-xl mx-auto flex flex-col gap-8 w-full mt-8">
+      <h1 className="text-white font-black text-5xl text-center">
+        Email List Formatter
+      </h1>
+      <section className="flex flex-col w-full gap-6 ">
+        <form
+          onSubmit={() => formatData(data, event)}
+          className={`flex flex-col justify-around gap-1.5 w-full`}
+        >
+          <textarea
+            className={`min-h-[200px] rounded cursor-auto outline w-full outline-[#50585F] outline-1 bg-[#0D1117] text-[#C9D1D8] p-1 text-sm focus:outline-blue-500`}
+            autoFocus
+            placeholder="Paste your email list here!"
+            onChange={(e) => setData(e.target.value)}
+            value={data}
+          ></textarea>
+          <button
+            type="submit"
+            className="bg-green-700 text-white text-sm py-2 rounded-md hover:bg-green-600 transition-colors"
           >
-            <textarea
-              className={`min-h-[200px] cursor-auto outline outline-white outline-1 bg-[#181818] text-white p-1 text-sm focus:outline-blue-500`}
-              autoFocus
-              placeholder="Paste your email list here!"
-              onChange={(e) => setData(e.target.value)}
-              value={data}
-            ></textarea>
-            <button
-              type="submit"
-              className="bg-white text-sm"
-            >
-              Submit
-            </button>
-          </form>
-          <div
-            className={`flex flex-col justify-around gap-1.5 w-full mx-auto`}
+            Submit
+          </button>
+        </form>
+        <div className={`flex flex-col justify-around gap-1.5 w-full`}>
+          <textarea
+            className="w-full bg-gray-800 rounded min-h-[200px] text-[#C9D1D8] p-1 text-sm"
+            placeholder="You'll see your results here!"
+            disabled
+            value={results.join("\n")}
+          />
+          <button
+            type="button"
+            onClick={copyToClipboard}
+            className="bg-gray-700 text-white text-sm py-2 rounded-md hover:outline hover:outline-blue-500 hover:outline-2 "
           >
-            <textarea
-              className="w-full min-h-[200px] text-white p-1 text-sm"
-              placeholder="You'll see your results here!"
-              disabled
-              value={results.join("\n")}
-            />
-            <button
-              type="button"
-              onClick={copyToClipboard}
-              className="bg-white text-sm"
-            >
-              Copy
-            </button>
-          </div>
-        </section>
-      </main>
-    </>
+            Copy
+          </button>
+        </div>
+      </section>
+    </div>
   );
 }
